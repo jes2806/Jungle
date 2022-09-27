08ec33350dae69acde3f846164db103ac06bb9be
John Sweeney <jes2806@gmail.com>
2022-09-27 16:21:47 -0600

    add cypress test for homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('visit homepage', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+})
